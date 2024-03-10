9a32a508f26b1ad2deb01e2a78c2fae5062a0f34
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-03-10 09:32:05 -0400

    seeding done

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,9 +1,33 @@
 const prisma = require ("../prisma");
 
 const seed = async () => {
-    // make 10 reservations
+    // make 10 customers
     for (let i =1; i < 10; i++){
         await prisma.customer.upsert({
+            where: {id: i},
+            update: {},
+            create: {
+                name: 'Customer ${i}'
+            },
+
+        });
+    }
+
+    // make 10 resturants
+    for (let i =1; i < 10; i++){
+        await prisma.restaurant.upsert({
+            where: {id: i},
+            update: {},
+            create: {
+                name: ' resturants ${ i %4 }',
+            },
+
+        });
+    }
+
+    // make reservation
+    for (let i =1; i < 10; i++){
+        await prisma.reservation.upsert({
             where: {id: i},
             update: {},
             create: {
